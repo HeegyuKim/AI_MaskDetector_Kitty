7d3536bebe25f1802874ae3d0515dd407a217670
YoungSoo Kim <kiskys1@gmail.com>
2021-09-19 08:45:16 +0900

    update

--- openCV/dnnface.py
+++ openCV/dnnface.py
@@ -19,35 +19,38 @@ if net.empty():
     exit()
 
 while True:
-    _, frame = cap.read()
-    if frame is None:
-        break
+    ret, frame = cap.read()
+    
+    if ret:
+        blob = cv2.dnn.blobFromImage(frame, 1, (300, 300), (104, 177, 123))
+        net.setInput(blob)
+        detect = net.forward()
 
-    blob = cv2.dnn.blobFromImage(frame, 1, (300, 300), (104, 177, 123))
-    net.setInput(blob)
-    detect = net.forward()
+        detect = detect[0, 0, :, :]
+        (h, w) = frame.shape[:2]
 
-    detect = detect[0, 0, :, :]
-    (h, w) = frame.shape[:2]
+        for i in range(detect.shape[0]):
+            confidence = detect[i, 2]
+            if confidence < 0.5:
+                break
 
-    for i in range(detect.shape[0]):
-        confidence = detect[i, 2]
-        if confidence < 0.5:
+            x1 = int(detect[i, 3] * w)
+            y1 = int(detect[i, 4] * h)
+            x2 = int(detect[i, 5] * w)
+            y2 = int(detect[i, 6] * h)
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0))
+
+            label = 'Face: %4.3f' % confidence
+            cv2.putText(frame, label, (x1, y1 - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) == 27:
             break
 
-        x1 = int(detect[i, 3] * w)
-        y1 = int(detect[i, 4] * h)
-        x2 = int(detect[i, 5] * w)
-        y2 = int(detect[i, 6] * h)
+    else:
+        print('error')
 
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0))
-
-        label = 'Face: %4.3f' % confidence
-        cv2.putText(frame, label, (x1, y1 - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
-
-    cv2.imshow('frame', frame)
-
-    if cv2.waitKey(1) == 27:
-        break
-
-cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()	 
