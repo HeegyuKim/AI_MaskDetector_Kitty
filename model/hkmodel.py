cb2e3453b77b6e08c05bbc0479b9a7921f829a4d
Heegyu Kim <heekue83@gmail.com>
2021-09-25 03:11:42 +0000

    pytorch-facenet과 직접 구현한 facenet 모델로 영상에서 얼굴찾기를 해봤지만 결과는 비슷했음

--- model/hkmodel.py
+++ model/hkmodel.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 
 from keras.preprocessing import image
 
-def preprocess_img(img_path, target_size=128):
-    img = image.load_img(img_path, target_size=(target_size, target_size))
+
+def normalize_img(img):
     rgb_tensor = tf.convert_to_tensor(img, dtype=tf.float32)
     rgb_tensor /= 255.
     return rgb_tensor
@@ -12,6 +12,7 @@ class SimpleCNNModel(tf.keras.models.Sequential):
     
     def __init__(self, def_target_size=128):
         super(SimpleCNNModel, self).__init__([
+            tf.keras.layers.Lambda(normalize_img),
             tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(def_target_size, def_target_size, 3)),
             tf.keras.layers.MaxPooling2D(2,2),
             tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
@@ -22,4 +23,3 @@ class SimpleCNNModel(tf.keras.models.Sequential):
             tf.keras.layers.Dense(32, activation='relu'),
             tf.keras.layers.Dense(2, activation='softmax')
         ])
-        
